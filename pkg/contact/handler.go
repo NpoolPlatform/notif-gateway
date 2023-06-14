8ae9e87f9c15a14fd5da60068db9c0b2040baed7
daqiangli <dqli161@163.com>
2023-06-14 15:40:23 +0800

    add contact handler

---
+++ pkg/contact/handler.go
@@ -0,0 +1,136 @@
+package contact
+
+import (
+	"context"
+	"fmt"
+
+	appcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/app"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	constant "github.com/NpoolPlatform/notif-gateway/pkg/const"
+	"github.com/google/uuid"
+)
+
+type Handler struct {
+	ID          *uuid.UUID
+	AppID       *uuid.UUID
+	UsedFor     *basetypes.UsedFor
+	AccountType *basetypes.SignMethod
+	Account     *string
+	Sender      *string
+	Offset      int32
+	Limit       int32
+}
+
+func NewHandler(ctx context.Context, options ...interface{}) (*Handler, error) {
+	handler := &Handler{}
+	for _, opt := range options {
+		_opt, ok := opt.(func(context.Context, *Handler) error)
+		if !ok {
+			continue
+		}
+		if err := _opt(ctx, handler); err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
+}
+
+func WithID(id *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.ID = &_id
+		return nil
+	}
+}
+
+func WithAppID(appID *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		_appID, err := uuid.Parse(*appID)
+		if err != nil {
+			return err
+		}
+		exist, err := appcli.ExistApp(ctx, *appID)
+		if err != nil {
+			return err
+		}
+		if !exist {
+			return fmt.Errorf("invalid app")
+		}
+
+		h.AppID = &_appID
+		return nil
+	}
+}
+
+func WithAccount(account *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if account == nil {
+			return nil
+		}
+		if *account == "" {
+			return fmt.Errorf("account is empty")
+		}
+		h.Account = account
+		return nil
+	}
+}
+
+func WithSender(sender *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if sender == nil {
+			return nil
+		}
+		if *sender == "" {
+			return fmt.Errorf("sender is empty")
+		}
+		h.Sender = sender
+		return nil
+	}
+}
+
+func WithUsedFor(usedFor *basetypes.UsedFor) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		switch *usedFor {
+		case basetypes.UsedFor_Contact:
+		default:
+			return fmt.Errorf("used for %v invalid", *usedFor)
+		}
+		h.UsedFor = usedFor
+		return nil
+	}
+}
+
+func WithAccountType(_type *basetypes.SignMethod) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if _type == nil {
+			return nil
+		}
+		switch *_type {
+		case basetypes.SignMethod_Email:
+		default:
+			return fmt.Errorf("type %v invalid", *_type)
+		}
+		h.AccountType = _type
+		return nil
+	}
+}
+
+func WithOffset(offset int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Offset = offset
+		return nil
+	}
+}
+
+func WithLimit(limit int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if limit == 0 {
+			limit = constant.DefaultRowLimit
+		}
+		h.Limit = limit
+		return nil
+	}
+}

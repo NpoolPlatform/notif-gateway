14b0174e0b693e198d37088c23881fe0fc9a8ff3
jakys <jakysing@163.com>
2023-06-15 13:12:35 +0000

    Refactor frontend

---
+++ pkg/template/frontend/query.go
@@ -0,0 +1,22 @@
+package frontend
+
+import (
+	"context"
+
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	frontendtemplatemwpb "github.com/NpoolPlatform/message/npool/notif/mw/v1/template/frontend"
+	frontendtemplatemwcli "github.com/NpoolPlatform/notif-middleware/pkg/client/template/frontend"
+)
+
+func (h *Handler) GetFrontendTemplate(ctx context.Context) (*frontendtemplatemwpb.FrontendTemplate, error) {
+	return frontendtemplatemwcli.GetFrontendTemplate(ctx, *h.ID)
+}
+
+func (h *Handler) GetFrontendTemplates(ctx context.Context) ([]*frontendtemplatemwpb.FrontendTemplate, uint32, error) {
+	return frontendtemplatemwcli.GetFrontendTemplates(ctx, &frontendtemplatemwpb.Conds{
+		AppID: &basetypes.StringVal{Op: cruder.EQ, Value: *h.AppID}},
+		h.Offset,
+		h.Limit,
+	)
+}

bad60d41ee640f0b5f347f99523eed733775faee
jakys <jakysing@163.com>
2023-06-15 08:04:25 +0000

    Refactor notif

--- pkg/notif/query.go
+++ pkg/notif/query.go
@@ -7,54 +7,45 @@ import (
 	appmwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/app"
 	usermwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
 	applangmwcli "github.com/NpoolPlatform/g11n-middleware/pkg/client/applang"
-
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	appmwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/app"
 	usermwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/user"
-	applangmwpb "github.com/NpoolPlatform/message/npool/g11n/mw/v1/applang"
-
-	mgrpb "github.com/NpoolPlatform/message/npool/notif/mgr/v1/notif"
-	mgrcli "github.com/NpoolPlatform/notif-manager/pkg/client/notif"
-
-	mwcli "github.com/NpoolPlatform/notif-middleware/pkg/client/notif"
-
-	npool "github.com/NpoolPlatform/message/npool/notif/gw/v1/notif"
-
-	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	applangmwpb "github.com/NpoolPlatform/message/npool/g11n/mw/v1/applang"
+	npool "github.com/NpoolPlatform/message/npool/notif/gw/v1/notif"
+	notifmwpb "github.com/NpoolPlatform/message/npool/notif/mw/v1/notif"
+	notifmwcli "github.com/NpoolPlatform/notif-middleware/pkg/client/notif"
 )
 
-func GetNotif(ctx context.Context, id string) (*npool.Notif, error) {
-	info, err := mgrcli.GetNotif(ctx, id)
+func (h *Handler) GetNotif(ctx context.Context) (*npool.Notif, error) {
+	info, err := notifmwcli.GetNotifOnly(ctx, &notifmwpb.Conds{
+		ID: &basetypes.StringVal{Op: cruder.EQ, Value: *h.ID},
+	})
 	if err != nil {
 		return nil, err
 	}
-
-	appInfo, err := appmwcli.GetApp(ctx, info.AppID)
+	app, err := appmwcli.GetApp(ctx, *h.AppID)
 	if err != nil {
 		return nil, err
 	}
-
-	if appInfo == nil {
-		return nil, fmt.Errorf("app %s not found", info.AppID)
+	if app == nil {
+		return nil, fmt.Errorf("app %s not found", h.AppID)
 	}
-
-	userInfo, err := usermwcli.GetUser(ctx, info.AppID, info.UserID)
+	user, err := usermwcli.GetUser(ctx, *h.AppID, *h.UserID)
 	if err != nil {
 		return nil, err
 	}
-
-	if userInfo == nil {
-		return nil, fmt.Errorf("user %s not found", info.UserID)
+	if user == nil {
+		return nil, fmt.Errorf("user %s not found", h.UserID)
 	}
-
 	return &npool.Notif{
 		ID:           info.ID,
 		AppID:        info.AppID,
-		AppName:      appInfo.Name,
+		AppName:      app.Name,
 		UserID:       info.UserID,
-		EmailAddress: userInfo.EmailAddress,
-		PhoneNO:      userInfo.PhoneNO,
-		Username:     userInfo.Username,
+		EmailAddress: user.EmailAddress,
+		PhoneNO:      user.PhoneNO,
+		Username:     user.Username,
 		EventType:    info.EventType,
 		UseTemplate:  info.UseTemplate,
 		Title:        info.Title,
@@ -66,85 +57,18 @@ func GetNotif(ctx context.Context, id string) (*npool.Notif, error) {
 	}, nil
 }
 
-func UpdateNotifs(ctx context.Context, ids []string, notified bool) ([]*npool.Notif, error) {
-	rows, err := mwcli.UpdateNotifs(ctx, ids, notified)
-	if err != nil {
-		return nil, err
+func (h *Handler) GetNotifs(ctx context.Context) ([]*npool.Notif, uint32, error) {
+	conds := &notifmwpb.Conds{}
+	if len(h.IDs) > 0 {
+		conds.IDs = &basetypes.StringSliceVal{Op: cruder.IN, Value: h.IDs}
 	}
-
-	if len(rows) == 0 {
-		return nil, nil
-	}
-
-	appIDs := []string{}
-	userIDs := []string{}
-
-	for _, val := range rows {
-		appIDs = append(appIDs, val.AppID)
-		userIDs = append(userIDs, val.UserID)
-	}
-	appInfos, _, err := appmwcli.GetApps(ctx, &appmwpb.Conds{
-		IDs: &basetypes.StringSliceVal{Op: cruder.IN, Value: appIDs},
-	}, 0, int32(len(appIDs)))
-	if err != nil {
-		return nil, err
-	}
-	appMap := map[string]*appmwpb.App{}
-	for _, val := range appInfos {
-		appMap[val.ID] = val
-	}
-
-	userInfos, _, err := usermwcli.GetUsers(ctx, &usermwpb.Conds{
-		IDs: &basetypes.StringSliceVal{Op: cruder.IN, Value: userIDs},
-	}, 0, int32(len(userIDs)))
-	if err != nil {
-		return nil, err
-	}
-	userMap := map[string]*usermwpb.User{}
-	for _, val := range userInfos {
-		userMap[val.ID] = val
-	}
-
-	infos := []*npool.Notif{}
-	for _, val := range rows {
-		app, ok := appMap[val.AppID]
-		if !ok {
-			continue
-		}
-		user, ok := userMap[val.UserID]
-		if !ok {
-			continue
-		}
-
-		infos = append(infos, &npool.Notif{
-			ID:           val.ID,
-			AppID:        val.AppID,
-			AppName:      app.Name,
-			UserID:       val.UserID,
-			EmailAddress: user.EmailAddress,
-			PhoneNO:      user.PhoneNO,
-			Username:     user.Username,
-			EventType:    val.EventType,
-			UseTemplate:  val.UseTemplate,
-			Title:        val.Title,
-			Content:      val.Content,
-			Channel:      val.Channel,
-			Notified:     val.Notified,
-			CreatedAt:    val.CreatedAt,
-			UpdatedAt:    val.UpdatedAt,
-		})
-	}
-	return infos, nil
-}
-
-func GetNotifs(ctx context.Context, conds *mgrpb.Conds, offset, limit int32) ([]*npool.Notif, uint32, error) {
-	rows, total, err := mgrcli.GetNotifs(ctx, conds, offset, limit)
+	rows, total, err := notifmwcli.GetNotifs(ctx, conds, h.Offset, h.Limit)
 	if err != nil {
 		return nil, 0, err
 	}
 
 	if len(rows) == 0 {
-		return nil, total, nil
+		return nil, 0, nil
 	}
 
 	appIDs := []string{}
@@ -179,7 +103,7 @@ func GetNotifs(ctx context.Context, conds *mgrpb.Conds, offset, limit int32) ([]
 	}
 
 	langs, _, err := applangmwcli.GetLangs(ctx, &applangmwpb.Conds{
-		AppID:   &basetypes.StringVal{Op: cruder.EQ, Value: conds.GetAppID().GetValue()},
+		AppID:   conds.AppID,
 		LangIDs: &basetypes.StringSliceVal{Op: cruder.IN, Value: langIDs},
 	}, 0, int32(len(langIDs)))
 	if err != nil {
@@ -192,7 +116,6 @@ func GetNotifs(ctx context.Context, conds *mgrpb.Conds, offset, limit int32) ([]
 	}
 
 	infos := []*npool.Notif{}
-
 	for _, val := range rows {
 		app, ok := appMap[val.AppID]
 		if !ok {
@@ -228,4 +151,5 @@ func GetNotifs(ctx context.Context, conds *mgrpb.Conds, offset, limit int32) ([]
 		})
 	}
 	return infos, total, nil
+
 }

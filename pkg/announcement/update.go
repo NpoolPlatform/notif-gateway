cd5dfc471bccd3703a6cf51ad9729f54ce45fdad
daqiangli <dqli161@163.com>
2023-06-16 17:06:49 +0800

    change EndAt type

--- pkg/announcement/update.go
+++ pkg/announcement/update.go
@@ -3,16 +3,17 @@ package announcement
 import (
 	"context"
 
-	npool "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement"
+	npool "github.com/NpoolPlatform/message/npool/notif/gw/v1/announcement"
+	mwpb "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement"
 	cli "github.com/NpoolPlatform/notif-middleware/pkg/client/announcement"
 )
 
 func (h *Handler) UpdateAnnouncement(ctx context.Context) (*npool.Announcement, error) {
-	_, err := cli.UpdateAnnouncement(ctx, &npool.AnnouncementReq{
+	_, err := cli.UpdateAnnouncement(ctx, &mwpb.AnnouncementReq{
 		ID:               h.ID,
 		Title:            h.Title,
 		Content:          h.Content,
-		EndAt:            &h.EndAt,
+		EndAt:            h.EndAt,
 		AnnouncementType: h.Type,
 	},
 	)

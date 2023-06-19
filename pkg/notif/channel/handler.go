c6e0e929c9a3275508c534f2bd88e5eb347e26b2
daqiangli <dqli161@163.com>
2023-06-19 16:28:51 +0800

    bugfix: #1531

--- pkg/notif/channel/handler.go
+++ pkg/notif/channel/handler.go
@@ -62,6 +62,9 @@ func WithAppID(appID *string) func(context.Context, *Handler) error {
 
 func WithChannel(channel *basetypes.NotifChannel) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
+		if channel == nil {
+			return fmt.Errorf("invalid channel")
+		}
 		switch *channel {
 		case basetypes.NotifChannel_ChannelEmail:
 		case basetypes.NotifChannel_ChannelSMS:
@@ -109,7 +112,7 @@ func WithEventTypes(_types []basetypes.UsedFor) func(context.Context, *Handler) 
 			case basetypes.UsedFor_KYCRejected:
 			case basetypes.UsedFor_Announcement:
 			default:
-				return fmt.Errorf("EventType is invalid")
+				return fmt.Errorf("event type is invalid")
 			}
 		}
 

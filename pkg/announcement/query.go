d2a8e5683d2dae6582516783241b8117aeacc258
daqiangli <dqli161@163.com>
2023-06-19 11:52:38 +0800

    fix amt bug

--- pkg/announcement/query.go
+++ pkg/announcement/query.go
@@ -11,7 +11,6 @@ import (
 	npool "github.com/NpoolPlatform/message/npool/notif/gw/v1/announcement"
 	mwpb "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement"
 	readpb "github.com/NpoolPlatform/message/npool/notif/mw/v1/announcement/readstate"
-	cli "github.com/NpoolPlatform/notif-middleware/pkg/client/announcement"
 	mwcli "github.com/NpoolPlatform/notif-middleware/pkg/client/announcement"
 	readcli "github.com/NpoolPlatform/notif-middleware/pkg/client/announcement/readstate"
 )
@@ -44,32 +43,66 @@ func (h *Handler) GetAnnouncements(ctx context.Context) ([]*npool.Announcement, 
 		return nil, total, nil
 	}
 
-	rows, err := formalize(ctx, infos[0].AppID, infos[0].UserID, infos)
-	if err != nil {
-		return nil, 0, err
+	if h.UserID != nil {
+		rows, err := formalize(ctx, *h.AppID, *h.UserID, infos)
+		if err != nil {
+			return nil, 0, err
+		}
+		return rows, total, nil
 	}
 
+	rows := []*npool.Announcement{}
+	for _, amt := range infos {
+		row := &npool.Announcement{
+			ID:               amt.ID,
+			AppID:            amt.AppID,
+			UserID:           "",
+			EmailAddress:     "",
+			PhoneNO:          "",
+			Username:         "",
+			LangID:           amt.LangID,
+			Title:            amt.Title,
+			Content:          amt.Content,
+			EndAt:            amt.EndAt,
+			Notified:         false,
+			CreatedAt:        amt.CreatedAt,
+			UpdatedAt:        amt.UpdatedAt,
+			Channel:          amt.Channel,
+			AnnouncementType: amt.AnnouncementType,
+		}
+		rows = append(rows, row)
+	}
 	return rows, total, nil
 }
 
 func (h *Handler) GetAnnouncement(ctx context.Context) (*npool.Announcement, error) {
-	info, err := cli.GetAnnouncement(ctx, *h.ID)
+	amt, err := mwcli.GetAnnouncement(ctx, *h.ID)
 	if err != nil {
 		return nil, err
 	}
-	if info == nil {
+	if amt == nil {
 		return nil, fmt.Errorf("announcement not exist")
 	}
 
-	infos, err := formalize(ctx, info.AppID, info.UserID, []*mwpb.Announcement{info})
-	if err != nil {
-		return nil, err
-	}
-	if len(infos) == 0 {
-		return nil, nil
+	info := &npool.Announcement{
+		ID:               amt.ID,
+		AppID:            amt.AppID,
+		UserID:           "",
+		EmailAddress:     "",
+		PhoneNO:          "",
+		Username:         "",
+		LangID:           amt.LangID,
+		Title:            amt.Title,
+		Content:          amt.Content,
+		EndAt:            amt.EndAt,
+		Notified:         false,
+		CreatedAt:        amt.CreatedAt,
+		UpdatedAt:        amt.UpdatedAt,
+		Channel:          amt.Channel,
+		AnnouncementType: amt.AnnouncementType,
 	}
 
-	return infos[0], nil
+	return info, nil
 }
 
 func formalize(ctx context.Context, appID, userID string, amts []*mwpb.Announcement) ([]*npool.Announcement, error) {
@@ -78,10 +111,8 @@ func formalize(ctx context.Context, appID, userID string, amts []*mwpb.Announcem
 	}
 
 	amtIDs := []string{}
-	userIDs := []string{}
 	for _, amt := range amts {
 		amtIDs = append(amtIDs, amt.ID)
-		userIDs = append(userIDs, amt.UserID)
 	}
 	if len(amtIDs) == 0 {
 		return nil, nil
@@ -111,6 +142,7 @@ func formalize(ctx context.Context, appID, userID string, amts []*mwpb.Announcem
 	}
 
 	// get users
+	userIDs := []string{userID}
 	userMap := map[string]*usermwpb.User{}
 	if len(userIDs) > 0 {
 		userInfos, _, err := usermwcli.GetUsers(ctx, &usermwpb.Conds{
@@ -126,20 +158,22 @@ func formalize(ctx context.Context, appID, userID string, amts []*mwpb.Announcem
 	}
 
 	announcements := []*npool.Announcement{}
+	user, ok := userMap[userID]
+	if !ok {
+		return nil, fmt.Errorf("invalid user id")
+	}
+
 	for _, amt := range amts {
 		notified := true
 		_, ok := readMap[amt.ID]
 		if !ok {
 			notified = false
 		}
-		user, ok := userMap[amt.UserID]
-		if !ok {
-			continue
-		}
+
 		announcements = append(announcements, &npool.Announcement{
 			ID:               amt.ID,
 			AppID:            amt.AppID,
-			UserID:           amt.UserID,
+			UserID:           user.ID,
 			EmailAddress:     user.EmailAddress,
 			PhoneNO:          user.PhoneNO,
 			Username:         user.Username,

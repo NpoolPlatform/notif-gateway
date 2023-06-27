efe32a5de5e45d8e7bebb41ad0fb616bf9f6738c
daqiangli <dqli161@163.com>
2023-06-27 15:14:03 +0800

    fix lint

--- pkg/notif/channel/create.go
+++ pkg/notif/channel/create.go
@@ -36,7 +36,7 @@ func (h *Handler) CreateChannel(ctx context.Context) ([]*npool.Channel, error) {
 
 		info, err := cli.CreateChannel(ctx, &npool.ChannelReq{
 			AppID:     h.AppID,
-			EventType: &eventType,
+			EventType: &eventType, //nolint
 			Channel:   h.Channel,
 		})
 		if err != nil {

d9c51e84d7a91baeb074d430c7fecd80832105f4
jakys <jakysing@163.com>
2023-11-03 08:34:40 +0000

    Add user check

--- pkg/notif/user/create.go
+++ pkg/notif/user/create.go
@@ -2,13 +2,23 @@ package user
 
 import (
 	"context"
+	"fmt"
 
+	usermwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
 	npool "github.com/NpoolPlatform/message/npool/notif/gw/v1/notif/user"
 	mwpb "github.com/NpoolPlatform/message/npool/notif/mw/v1/notif/user"
 	cli "github.com/NpoolPlatform/notif-middleware/pkg/client/notif/user"
 )
 
 func (h *Handler) CreateNotifUser(ctx context.Context) (*npool.NotifUser, error) {
+	existUser, err := usermwcli.ExistUser(ctx, *h.AppID, *h.UserID)
+	if err != nil {
+		return nil, err
+	}
+	if !existUser {
+		return nil, fmt.Errorf("invalid user")
+	}
+
 	info, err := cli.CreateNotifUser(
 		ctx,
 		&mwpb.NotifUserReq{
